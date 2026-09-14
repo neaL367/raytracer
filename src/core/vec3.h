d736e81c3216851c270a09125c528fdab843dbdb
neaL367 <atichatfantasy@gmail.com>
2026-09-15 05:00:18 +0700

    feat: add vec3 addition operator

--- src/core/vec3.h
+++ src/core/vec3.h
@@ -1,10 +1,6 @@
 #pragma once
 
-class vec3
-{
-private:
-    double e[3];
-
+class vec3 {
 public:
     vec3() : e{0, 0, 0} {}
     vec3(double x, double y, double z) : e{x, y, z} {}
@@ -12,4 +8,11 @@ public:
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
+
+private:
+    double e[3];
 };
+
+vec3 operator+(const vec3& a, const vec3& b) {
+    return vec3(a.x() + b.x(), a.y() + b.y(), a.z() + b.z());
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int main()
 {
     vec3 v(5.0, 3.0, 2.0);
 
+    vec3 operator_plus = v + vec3(1.0, 2.0, 3.0);
+    std::cout << operator_plus.x() << ", " << operator_plus.y() << ", " << operator_plus.z() << "\n";
+    
     const int width = 256;
     const int height = 256;
 
