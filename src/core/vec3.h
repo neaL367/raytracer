ca2950ceb28571e402c2f7d99e3e68a2d6d3260b
neaL367 <atichatfantasy@gmail.com>
2026-09-15 05:16:16 +0700

    feat: add vec3 compound assignment operators

--- src/core/vec3.h
+++ src/core/vec3.h
@@ -1,6 +1,7 @@
 #pragma once
 
-class vec3 {
+class vec3
+{
 public:
     vec3() : e{0, 0, 0} {}
     vec3(double x, double y, double z) : e{x, y, z} {}
@@ -9,10 +10,19 @@ public:
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
+    vec3 &operator+=(const vec3 &other)
+    {
+        e[0] += other.x();
+        e[1] += other.y();
+        e[2] += other.z();
+        return *this;
+    }
+
 private:
     double e[3];
 };
 
-vec3 operator+(const vec3& a, const vec3& b) {
-    return vec3(a.x() + b.x(), a.y() + b.y(), a.z() + b.z());
+vec3 operator+(const vec3 &lhs, const vec3 &rhs)
+{
+    return vec3(lhs.x() + rhs.x(), lhs.y() + rhs.y(), lhs.z() + rhs.z());
 }
